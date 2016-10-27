137458580b9bfabfa8f59e7a606b71d9ffe96856
Ethan Higa <ewh1901@iolani.org>
2016-10-26 16:06:34 -1000

    Drivetrain,Catapult

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -18,6 +18,9 @@ public class ColorSensorTest extends OpMode {
     DeviceInterfaceModule cdim;
 
     static final int LED_CHANNEL = 5;
+    int redC;
+    int greenC;
+    int blueC;
 
     @Override
     public void init() {
@@ -25,11 +28,12 @@ public class ColorSensorTest extends OpMode {
         cdim = hardwareMap.deviceInterfaceModule.get("dim");
         cdim.setDigitalChannelMode(LED_CHANNEL, DigitalChannelController.Mode.OUTPUT);
         cdim.setDigitalChannelState(LED_CHANNEL, false);
+
     }
 
     @Override
     public void loop() {
-        telemetry.addData("red", colorSensorL.red());
+        telemetry.addData("red", colorSensorL.red()); //21504
         telemetry.addData("green", colorSensorL.green());
         telemetry.addData("blue", colorSensorL.blue());
         //telemetry.addData("color", ) // TODO - Differentiate between red and blue
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestRobotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestRobotTeleOp.java
@@ -1,20 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by Teacher on 9/28/2016.
  */
-
-@Autonomous(name="TestRobot: Teleop", group="TestRobot")
+//Noting Code, not for code
+    //When you turn on the robot the FTC app will automatically open.
+    //Start+A button on remote to start TeleOp
+@TeleOp(name="TestRobot: Teleop", group="TestRobot") //Alt enter to import code
 //@Disabled
 public class TestRobotTeleOp extends OpMode {
     DcMotor frontLeftMotor;
     DcMotor frontRightMotor;
     DcMotor backLeftMotor;
     DcMotor backRightMotor;
+    //Servo wing;
 
     @Override
     public void init() {
@@ -22,6 +26,7 @@ public class TestRobotTeleOp extends OpMode {
         frontRightMotor = hardwareMap.dcMotor.get("rightFront");
         backLeftMotor = hardwareMap.dcMotor.get("leftRear");
         backRightMotor = hardwareMap.dcMotor.get("rightRear");
+       // wing = hardwareMap.servo.get(""); // TODO - Name hardware
 
         frontLeftMotor.setDirection(DcMotor.Direction.REVERSE); //Motors will normally start at way that is not to good
         backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
@@ -37,5 +42,8 @@ public class TestRobotTeleOp extends OpMode {
 
         telemetry.addData("Front Left Power", frontLeftMotor.getPowerFloat()); //String is objective and getting power of motor
         telemetry.update(); //Adds data to phone
+        if(gamepad1.left_bumper) {
+
+        }
     }
 }
